216e78e030aea55ee66bc0295e8da0cf60719cf1
anshul5975-dot <anshulsharma4864@gmail.com>
2026-01-28 14:03:11 +0530

    first commit

---
+++ src/products/SearchProduct.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { useForm } from 'react-hook-form'
+
+export default function SearchProducts(props) {
+    let { register, handleSubmit, formState } = useForm()
+    function collectFormData(data)
+    {
+        console.log(data);
+        props.searchProductsByNameFunction(data.productName)
+        
+    }
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit(collectFormData)}>
+                <div className="mb-3 d-flex">
+                    <input type="text" className="form-control" placeholder='Enter product name'
+                    {...register("ProductName", {required:true,message:"username is required"})}/>
+                    {formState.errors?.productName?alert("product name is required"):""}
+                    <input type="submit" className="btn btn-primary ms-2"/>
+                </div> 
+                
+                
+            </form>
+        </div>
+    )
+}
